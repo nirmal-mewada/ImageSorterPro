4960e274f19fcc970a3ad231d6e76dc7a6b7a3ae
Nirmal <nirmalmewada@gmail.com>
2026-06-21 00:50:24 +0530

    Add Main class launcher, update POM and release workflow to use Main to bypass JavaFX launch checks

---
+++ src/main/java/com/imagesorter/Main.java
@@ -0,0 +1,10 @@
+package com.imagesorter;
+
+/**
+ * Standard launcher class to bypass JavaFX application startup checks.
+ */
+public class Main {
+    public static void main(String[] args) {
+        ImageSorterApp.main(args);
+    }
+}
